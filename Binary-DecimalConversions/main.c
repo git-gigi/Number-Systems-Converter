023324fa9c6823789f76e8f0a53da68f9a3c9e37
Gigi <andreabotticelli08@gmail.com>
2023-11-01 01:31:40 +0100

    Initial Commit

---
+++ Binary-DecimalConversions/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Binary-DecimalConversions
+//
+//  Created by Luigi Meola on 01/11/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
