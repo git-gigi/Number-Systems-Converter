80f4d6671ae34ffe310c487b926580d0692c557a
Gigi <andreabotticelli08@gmail.com>
2023-11-01 01:37:02 +0100

    first commit

---
+++ Binary-DecimalConversions/Binary-DecimalConversions.h
@@ -0,0 +1,16 @@
+//
+//  Binary-DecimalConversions.h
+//  Binary-DecimalConversions
+//
+//  Created by Luigi Meola on 01/11/23.
+//
+
+#ifndef Binary_DecimalConversions_h
+#define Binary_DecimalConversions_h
+
+#include <stdio.h>
+#define BITMAX 256
+int decimalToBinary(int D, int N);
+int binaryToDecimal(char binaryNum[BITMAX]);
+
+#endif /* Binary_DecimalConversions_h */
--- Binary-DecimalConversions/main.c
+++ Binary-DecimalConversions/main.c
@@ -5,10 +5,42 @@
 //  Created by Luigi Meola on 01/11/23.
 //
 
+//Convertitore Decimale-Binario con numero di Bit
 #include <stdio.h>
+#include "Binary-DecimalConversions.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+int main (void)
+{
+    int b;
+    printf("Inserisci 0 per convertire da decimale a binario, inserisci 1 per convertire da binario a decimale: \n");
+    scanf("%d", &b);
+    scanf("%*c");
+    
+    if (b == 0) {
+        int D, N;
+        printf("Inserisci il numero da trasformare in binario: \n");
+        scanf("%d", &D);
+        printf("Inserisci il numero di bit: \n");
+        scanf("%d", &N);
+        
+        decimalToBinary(D, N);
+    }
+    else if (b == 1)
+    {
+        int i = 0;
+        char c;
+        char binaryNum[BITMAX];
+        printf("Inserisci numero binario: \n");
+        
+        do {
+            scanf("%c", &c);
+            binaryNum[i] = c;
+            i++;
+        } while (binaryNum[i-1] != '\n');
+        printf("%d\n", binaryToDecimal(binaryNum));
+    }
+    else
+        printf("Numero inserito non valido!");
 }
+
+
